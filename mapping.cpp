139ba9c568118fc3e6782721ebcb2803eceede67
Jerome Forissier <jerome@taodyne.com>
2011-05-27 11:17:45 +0200

    Fix build warning=error on gcc 4.2.1.

--- mapping.cpp
+++ mapping.cpp
@@ -27,7 +27,7 @@
 
 XL_DEFINE_TRACES
 
-Tree_p plane(Tree_p tree, Real_p x, Real_p y, Real_p w,
+Tree_p plane(Tree_p, Real_p x, Real_p y, Real_p w,
              Real_p h, Integer_p lines_nb, Integer_p columns_nb)
 // ----------------------------------------------------------------------------
 //    Define the lens flare target
