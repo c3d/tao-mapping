0063f703cbd15df5e130eaba36c484d1e38d7463
Christophe de Dinechin <christophe@taodyne.com>
2011-08-29 13:38:57 +0200

    Modified licence checks to use digests instead of crypto

--- mapping.cpp
+++ mapping.cpp
@@ -26,24 +26,19 @@ XL_DEFINE_TRACES
 
 CubeMap* cube;
 
-Tree_p plane(Tree_p,
-             Real_p x, Real_p y, Real_p w, Real_p h,
-             Integer_p rows_nb, Integer_p columns_nb)
+Tree_p plane(Tree_p, Real_p x, Real_p y, Real_p w,
+             Real_p h, Integer_p lines_nb, Integer_p columns_nb)
 // ----------------------------------------------------------------------------
 //   Define a subdivded plane
 // ----------------------------------------------------------------------------
 {
-    coord xx = x, yy = y, ww = w, hh = h;
-    int r = rows_nb, c = columns_nb;
-    Plane* plane = new Plane(xx, yy, ww, hh, r, c);
-    TextureMapping::tao->addToLayout(TextureMapping::render_callback, plane,
-                                     TextureMapping::delete_callback);
+    Plane* plane = new Plane(x, y, w, h, lines_nb, columns_nb);
+    TextureMapping::tao->addToLayout(TextureMapping::render_callback, plane, TextureMapping::delete_callback);
 
     return xl_true;
 }
 
-
-Tree_p texture_cube(Context_p context, Integer_p size, Tree_p, Tree_p prog)
+Tree_p texture_cube(Context *context, Integer_p size, Tree_p, Tree_p prog)
 // ----------------------------------------------------------------------------
 //   Create a cube map texture
 // ----------------------------------------------------------------------------
@@ -53,25 +48,23 @@ Tree_p texture_cube(Context_p context, Integer_p size, Tree_p, Tree_p prog)
 
     cube->loadCubeMap();
 
-    TextureMapping::tao->addToLayout(TextureMapping::render_callback, cube,
-                                     TextureMapping::delete_callback);
+    TextureMapping::tao->addToLayout(TextureMapping::render_callback, cube, TextureMapping::delete_callback);
 
     return xl_true;
 }
-
 
 Tree_p cube_map_face(Tree_p tree, GLuint face, text filename)
 // ----------------------------------------------------------------------------
 //   Add texture to the current cube map
 // ----------------------------------------------------------------------------
 {
-    if (!cube)
+    if(! cube)
     {
         Ooops("No mapping defined '$1' ", tree);
         return xl_false;
     }
 
-    if (!cube->setTexture( filename, face))
+    if(! cube->setTexture( filename, face))
     {
         Ooops("No correct face '$1' ", tree);
         return xl_false;
@@ -80,13 +73,12 @@ Tree_p cube_map_face(Tree_p tree, GLuint face, text filename)
     return xl_true;
 }
 
-
 Tree_p cube_map_flip(Tree_p tree, bool u, bool v)
 // ----------------------------------------------------------------------------
 //   Allow to flip faces of the current cube map
 // ----------------------------------------------------------------------------
 {
-    if (!cube)
+    if(! cube)
     {
         Ooops("No mapping defined '$1' ", tree);
         return xl_false;
@@ -96,7 +88,6 @@ Tree_p cube_map_flip(Tree_p tree, bool u, bool v)
 
     return xl_true;
 }
-
 
 int module_init(const Tao::ModuleApi *api, const Tao::ModuleInfo *)
 // ----------------------------------------------------------------------------
@@ -108,7 +99,6 @@ int module_init(const Tao::ModuleApi *api, const Tao::ModuleInfo *)
 
     return 0;
 }
-
 
 int module_exit()
 // ----------------------------------------------------------------------------
--- mapping.h
+++ mapping.h
@@ -36,7 +36,7 @@ using namespace XL;
 
 Tree_p plane(Tree_p tree, Real_p x, Real_p y, Real_p w,
              Real_p h, Integer_p lines_nb, Integer_p columns_nb);
-Tree_p texture_cube(Context_p context, Integer_p size, Tree_p, Tree_p prog);
+Tree_p texture_cube(Context *context, Integer_p size, Tree_p, Tree_p prog);
 Tree_p cube_map_face(Tree_p tree, GLuint face, text name);
 Tree_p cube_map_flip(Tree_p tree, bool u, bool v);
 
