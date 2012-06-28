48b8a43a27f2e18437757fbe3abb221f513be47a
Jerome Forissier <jerome@taodyne.com>
2012-06-28 18:03:10 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao

--- doc/mapping.doxy.h
+++ doc/mapping.doxy.h
@@ -474,7 +474,6 @@ cube_mapping(ratio:integer);
 texture_unit 0
 texture "color_map.png"
 // Define sphere map for reflection
-texture_unit 1
 texture "sphere_map.png"
 rotatex time * 20
 rotatey 90
@@ -513,7 +512,6 @@ sphere 0, 0, 0, 500, 500, 500, 50, 50
 texture_unit 0
 texture "color_map.png"
 // Définition d'une sphere map pour simuler une réflexion
-texture_unit 1
 texture "sphere_map.png"
 rotatex time * 20
 rotatey 90
