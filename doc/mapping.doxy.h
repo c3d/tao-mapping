50859cd92dda19399d86654417f8a6d18f970d13
Catherine Burvelle <catherine@taodyne.com>
2012-07-19 09:40:52 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~jerome/Work/tao

--- doc/mapping.doxy.h
+++ doc/mapping.doxy.h
@@ -26,7 +26,7 @@
  * The example below presents a use case of displacement mapping.
  * @include displacement.ddd
  *
- * @image html displacement.png "Output from displacement.ddd"
+ * @image html displacement.png "Displacement mapping"
  *
  * @~french
  * Techniques de mapping.
@@ -474,6 +474,7 @@ cube_mapping(ratio:integer);
 texture_unit 0
 texture "color_map.png"
 // Define sphere map for reflection
+texture_unit 1
 texture "sphere_map.png"
 rotatex time * 20
 rotatey 90
@@ -512,6 +513,7 @@ sphere 0, 0, 0, 500, 500, 500, 50, 50
 texture_unit 0
 texture "color_map.png"
 // Définition d'une sphere map pour simuler une réflexion
+texture_unit 1
 texture "sphere_map.png"
 rotatex time * 20
 rotatey 90
